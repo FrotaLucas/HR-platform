22b59624eb36575fce8ccf35fcc0ce7e8eae1176
FrotaLucas <lucasdiasfrota@gmail.com>
2023-10-25 23:08:58 +0200

    develop method close to AgregarEditarPersonaComponent

--- src/app/components/agregar-editar-persona/agregar-editar-persona.component.ts
+++ src/app/components/agregar-editar-persona/agregar-editar-persona.component.ts
@@ -1,10 +1,20 @@
+import { OnInit} from '@angular/core'
 import { Component } from '@angular/core';
-
+import { MatDialogRef } from '@angular/material/dialog';
 @Component({
   selector: 'app-agregar-editar-persona',
   templateUrl: './agregar-editar-persona.component.html',
   styleUrls: ['./agregar-editar-persona.component.css']
 })
-export class AgregarEditarPersonaComponent {
+export class AgregarEditarPersonaComponent implements OnInit {
+
+  constructor( public dialogRef: MatDialogRef<AgregarEditarPersonaComponent>){
+  }
+  ngOnInit(): void {
+  }
+  methodCancell(){
+    const dialogRef = this.dialogRef.close()
+
+  }
 
 }
