e1f6787297120da62c3c3751c92e30647e869566
FrotaLucas <lucasdiasfrota@gmail.com>
2023-12-08 13:32:25 +0100

    implement obtenerPersona and disable arrray Person

--- src/app/components/list-personas/list-personas.component.ts
+++ src/app/components/list-personas/list-personas.component.ts
@@ -6,16 +6,17 @@ import { MatTableDataSource } from '@angular/material/table';
 import { Persona } from 'src/app/interfaces/persona';
 import { AgregarEditarPersonaComponent } from '../agregar-editar-persona/agregar-editar-persona.component';
 import { PersonaService } from 'src/app/services/persona.service';
+import { ProgressBarMode } from '@angular/material/progress-bar';
 
-const listPersonas: Persona[] = [
-  {nombre: "Maria", apellido: "Loya", correo: "loyadiasMaria@hotmail.com", tipoDocumento: 'CNH', documento: 3333333, fechaNacimento: new Date() },
-  {nombre: "Fagner", apellido: "Melles", correo: "fafafagner@hotmail.com", tipoDocumento: 'CNH', documento: 1414141, fechaNacimento: new Date() },
-  {nombre: "Jorge", apellido: "Paulo", correo: "jorge@hotmail.com", tipoDocumento: 'CNH', documento: 1414141, fechaNacimento: new Date() },
-  {nombre: "Iago", apellido: "Ottilis", correo: "claudia@hotmail.com", tipoDocumento: 'CNH', documento: 1414141, fechaNacimento: new Date() },
-  {nombre: "Grazi", apellido: "Carneiro", correo: "grazi@hotmail.com", tipoDocumento: 'CNH', documento: 1414141, fechaNacimento: new Date() },
-  {nombre: "Lucas", apellido: "Frota", correo: "frotadiaslucas@hotmail.com", tipoDocumento: 'CNH', documento: 21212121, fechaNacimento: new Date() },
-  {nombre: "Grazi", apellido: "Carneiro", correo: "grazi@hotmail.com", tipoDocumento: 'CNH', documento: 1414141, fechaNacimento: new Date() }
-];
+// const listPersonas: Persona[] = [
+//   {nombre: "Maria", apellido: "Loya", correo: "loyadiasMaria@hotmail.com", tipoDocumento: 'CNH', documento: 3333333, fechaNacimento: new Date() },
+//   {nombre: "Fagner", apellido: "Melles", correo: "fafafagner@hotmail.com", tipoDocumento: 'CNH', documento: 1414141, fechaNacimento: new Date() },
+//   {nombre: "Jorge", apellido: "Paulo", correo: "jorge@hotmail.com", tipoDocumento: 'CNH', documento: 1414141, fechaNacimento: new Date() },
+//   {nombre: "Iago", apellido: "Ottilis", correo: "claudia@hotmail.com", tipoDocumento: 'CNH', documento: 1414141, fechaNacimento: new Date() },
+//   {nombre: "Grazi", apellido: "Carneiro", correo: "grazi@hotmail.com", tipoDocumento: 'CNH', documento: 1414141, fechaNacimento: new Date() },
+//   {nombre: "Lucas", apellido: "Frota", correo: "frotadiaslucas@hotmail.com", tipoDocumento: 'CNH', documento: 21212121, fechaNacimento: new Date() },
+//   {nombre: "Grazi", apellido: "Carneiro", correo: "grazi@hotmail.com", tipoDocumento: 'CNH', documento: 1414141, fechaNacimento: new Date() }
+// ];
 
 @Component({
   selector: 'app-list-personas',
@@ -24,7 +25,10 @@ const listPersonas: Persona[] = [
 })
 export class ListPersonasComponent implements OnInit, AfterViewInit {
   displayedColumns: string[] = ['nombre', 'apellido', 'correo', 'tipoDocumento', 'documento', 'fechaNacimento', 'acoes'];
-  dataSource: MatTableDataSource<Persona> 
+  dataSource: MatTableDataSource<Persona>;
+  newmode: ProgressBarMode = 'indeterminate'
+  loading: boolean = true;
+
 
   //ache o componente MatPaginator e atribua ele a myCustomPaginator
   @ViewChild(MatPaginator) myCustomPaginator!: MatPaginator;
@@ -37,9 +41,15 @@ export class ListPersonasComponent implements OnInit, AfterViewInit {
     this.dataSource = new MatTableDataSource()
   }
   obtenerPersonas(){
-  this._personaService.getPersona().subscribe(data =>{
-  this.dataSource.data = data;
-  })
+      this.loading = true;
+      setTimeout(()=>{
+        this._personaService.getPersona().subscribe(data =>{
+        this.loading = false
+        this.dataSource.data = data;
+        this.dataSource.paginator = this.myCustomPaginator;
+        this.dataSource.sort = this.myCustomSort;
+          });
+      }, 2000);
   }
 
   applyFilter(event: Event){
@@ -57,12 +67,20 @@ export class ListPersonasComponent implements OnInit, AfterViewInit {
     dialogRef.afterClosed().subscribe(result => { console.log('The dialog was closed')})
   }
 
+  deletePerson(id: number){
+    this.loading = true;
+    this._personaService.deletePersona(id).subscribe(()=>{
+      this.obtenerPersonas()
+    })
+  }
   ngAfterViewInit(): void {
-    this.dataSource.paginator = this.myCustomPaginator;
+    //1 entender melhor esse paginator. Pq tiranto ele mesmo assim ele aparece ?
+    this.dataSource.paginator = this.myCustomPaginator; 
     this.dataSource.sort = this.myCustomSort;
     //this.dataSource.paginator._intl.itemsPerPageLabel = "items per pagina";
   }
   ngOnInit(): void {
+    // 2 Paginator aqui tb mesmo tirando ele de obtenerPersona, aparece aqui
     this.obtenerPersonas();
   }
 
