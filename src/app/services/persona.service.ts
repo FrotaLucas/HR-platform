5435572b97f5c58dad9dc359a24cd838950377d6
FrotaLucas <lucasdiasfrota@gmail.com>
2023-12-26 17:16:44 +0100

    add two new method getPersona and updatePersona

--- src/app/services/persona.service.ts
+++ src/app/services/persona.service.ts
@@ -28,6 +28,14 @@ export class PersonaService {
    addPersona(persona: Persona): Observable<void>{
     return this.http.post<void>(`${this.myAppUrl}${this.myApiUrl}`, persona)
    }
+
+   getPersona(id: number): Observable<Persona>{
+    return this.http.get<Persona>(`${this.myAppUrl}${this.myApiUrl}${id}`) //nao precisa do [] em Persona ?
+   }
    
+   updatePersona(id: number, persona: Persona): Observable<void>{
+    return this.http.put<void>(`${this.myAppUrl}${this.myApiUrl}${id}`, persona);
+   }
+
 
 }
