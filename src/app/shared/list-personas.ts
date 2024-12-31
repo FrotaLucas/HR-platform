7d4a5af7598446d31673877d8ce6add0ced33128
FrotaLucas <lucasdiasfrota@gmail.com>
2024-12-31 15:21:33 +0100

    create list with persons

---
+++ src/app/shared/list-personas.ts
@@ -0,0 +1,69 @@
+import { Persona } from '../interfaces/persona';
+
+const listDePersonas: Persona[] = [
+  {
+    id: 0,
+    nombre: 'Maria',
+    apellido: 'Loya',
+    correo: 'loyadiasMaria@hotmail.com',
+    tipoDocumento: 'CNH',
+    documento: 3333333,
+    fechaNacimento: new Date(),
+  },
+  {
+    id: 1,
+    nombre: 'Fagner',
+    apellido: 'Melles',
+    correo: 'fafafagner@hotmail.com',
+    tipoDocumento: 'CNH',
+    documento: 1414141,
+    fechaNacimento: new Date(),
+  },
+  {
+    id: 2,
+    nombre: 'Jorge',
+    apellido: 'Paulo',
+    correo: 'jorge@hotmail.com',
+    tipoDocumento: 'CNH',
+    documento: 1414141,
+    fechaNacimento: new Date(),
+  },
+  {
+    id: 3,
+    nombre: 'Iago',
+    apellido: 'Ottilis',
+    correo: 'claudia@hotmail.com',
+    tipoDocumento: 'CNH',
+    documento: 1414141,
+    fechaNacimento: new Date(),
+  },
+  {
+    id: 4,
+    nombre: 'Grazi',
+    apellido: 'Carneiro',
+    correo: 'grazi@hotmail.com',
+    tipoDocumento: 'CNH',
+    documento: 1414141,
+    fechaNacimento: new Date(),
+  },
+  {
+    id: 5,
+    nombre: 'Lucas',
+    apellido: 'Frota',
+    correo: 'frotadiaslucas@hotmail.com',
+    tipoDocumento: 'CNH',
+    documento: 21212121,
+    fechaNacimento: new Date(),
+  },
+  {
+    id: 6,
+    nombre: 'Grazi',
+    apellido: 'Carneiro',
+    correo: 'grazi@hotmail.com',
+    tipoDocumento: 'CNH',
+    documento: 1414141,
+    fechaNacimento: new Date(),
+  },
+];
+
+export { listDePersonas };
