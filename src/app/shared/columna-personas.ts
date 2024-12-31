ba2ab32be88521b796c7a64ca0c5ad2603df2dbd
FrotaLucas <lucasdiasfrota@gmail.com>
2024-12-31 15:24:25 +0100

    create list with column names

---
+++ src/app/shared/columna-personas.ts
@@ -0,0 +1,11 @@
+const columnaDePersonas: string[] = [
+  'nombre',
+  'apellido',
+  'correo',
+  'tipoDocumento',
+  'documento',
+  'fechaNacimento',
+  'acoes',
+];
+
+export { columnaDePersonas };
